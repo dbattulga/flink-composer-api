7b5a12080ff3a0f68ddb955d421fd9247e0bcea2
Davaadorj Battulga <davaadorj.battulga@u-hopper.com>
2020-04-16 15:50:59 +0200

    initial

---
+++ job_registry/__init__.py
@@ -0,0 +1,23 @@
+import markdown
+import os
+
+# Import the framework
+from flask import Flask
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Present some documentation"""
+
+    # Open the README file
+    with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
+
+        # Read the content of the file
+        content = markdown_file.read()
+
+        # Convert to HTML
+        return markdown.markdown(content)
+
