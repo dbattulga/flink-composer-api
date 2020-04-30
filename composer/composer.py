1d6d869a2e9c988ac7c400f56436629a554d156b
Davaadorj Battulga <davaadorj.battulga@u-hopper.com>
2020-04-30 13:29:45 +0200

    latest

--- composer/composer.py
+++ composer/composer.py
@@ -1,12 +1,47 @@
 import yaml
 import json
+import requests
+
+base_url = "http://localhost:5000"
+
+def get_jobs(url):
+    check = requests.get(url + "/jobs")
+    sysjobs = []
+    if (check.ok):
+        response = json.loads(check.content)
+        if len(response['data']) != 0:
+            for jobs in response['data']:
+                sysjobs.append(jobs['jobname'])
+    return sysjobs
+
+
 
 def run_conf():
     with open('userconf.yaml') as f:
         userconf = yaml.load(f)
+    sysjobs = get_jobs(base_url)
 
-    print(userconf)
+    userjobs = []
+    for userjob in userconf['jobs']:
+        userjobs.append(userjob['job_name'])
+        print("post: "+userjob['job_name'])
+        body = {'job_name': userjob['job_name'],
+                  'version': userjob['version'],
+                  'flink_address': userjob['flink_address'],
+                  'mqtt_address': userjob['mqtt_address'],
+                  'source_topic': userjob['source_topic'],
+                  'sink_topic': userjob['sink_topic'],
+                  'entry_class': userjob['entry_class'],
+                  'jar_path': userjob['jar_path']
+                  }
+        start = requests.post(base_url + "/jobs", json=body)
+        print(start.status_code)
 
+    for sysjob in sysjobs:
+        if sysjob not in userjobs:
+            print("delete: "+sysjob)
+            delete = requests.delete(base_url + "/jobs/" + sysjob)
+            print(delete.status_code)
 
 run_conf()
 
